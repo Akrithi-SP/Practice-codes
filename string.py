a549c551b9af075aa1937702d2177bc3e360f944
Akrithi-SP <90304253+Akrithi-SP@users.noreply.github.com>
2021-12-20 12:46:02 +0530

    Create string.py

---
+++ string.py
@@ -0,0 +1,15 @@
+'''
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+'''
+#print ('Hello World')
+s = input().split()
+#ns=0
+for i in s:
+    print(i[0],end="")
+#print(ns)
+
